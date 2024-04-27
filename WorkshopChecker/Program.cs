0772daa7f6df42b92b556ed4654e179c089d33f5
YaMang -w- <98645233+YaMang0w0@users.noreply.github.com>
2024-04-28 08:44:38 +0900

    Add files via upload

--- WorkshopChecker/Program.cs
+++ WorkshopChecker/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
+using Pastel;
+using System.Net.Http;
 
 namespace WorkshopChecker
 {
@@ -7,6 +9,10 @@ namespace WorkshopChecker
         static readonly string _path = $"{Directory.GetCurrentDirectory()}\\workshops.txt";
         private static readonly HttpClient client = new HttpClient();
         private static string _workshop = "";
+        private static int _workshopCount = 0;
+        private static int _vaildworkshopCount = 0;
+        private static int _invailedworkshopCount = 0;
+
         static void Main(string[] args)
         {
             if(hasFile())
@@ -31,10 +37,12 @@ namespace WorkshopChecker
             tasks.Add(Task.Run(async () =>
             {
                 string[] ids = _workshop.Split(';');
+                _workshopCount = ids.Count();
                 foreach (string id in ids)
                 {
                     await GetWorkshopInfo(id);
                 }
+                Console.WriteLine("END");
             }));
 
             await Task.WhenAll(tasks);
@@ -54,13 +62,45 @@ namespace WorkshopChecker
             };
 
             var r = await PostHTTPRequestAsync("https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/", a);
+            var rr = await GetHTTPRequestAsync($"https://steamcommunity.com/sharedfiles/filedetails/?id={id}");
+            var issue = "";
+            int startIndex = rr.IndexOf("<title>") + "<title>".Length;
+            int endIndex = rr.IndexOf("</title>");
+
+            // 중간 내용 추출
+            string middleContent = rr.Substring(startIndex, endIndex - startIndex);
 
             var json = JObject.Parse(r);
             if(json != null)
             {
                 var details = json["response"]["publishedfiledetails"][0];
 
-                Console.WriteLine($"{details["result"]} | {id} | {details["title"]}");
+                var title = details?["title"]?.ToString();
+
+                if (string.IsNullOrEmpty(title))
+                    title = $"https://steamcommunity.com/sharedfiles/filedetails/?id={id}";
+
+                string code = "";
+                if (details["result"].ToString() != "1")
+                    code = "[ Invalid | Check URL ]".Pastel("FF2100");
+                else
+                    code = "[ Valid ]".Pastel("00FFFF");
+                middleContent = middleContent.Replace("Steam Community :: ", "");
+                if (middleContent.Contains("Error"))
+                {
+                    title = "Error";
+                    issue = "There is no workshop page.";
+                    Console.WriteLine($"{code} WorkshopID: {id} | Workshop Title: {title} | Issue: {issue}".PastelBg("FF7800"));
+                    _invailedworkshopCount++;
+                }
+                else
+                {
+                    issue = "None";
+                    Console.WriteLine($"{code} WorkshopID: {id} | Workshop Title: {title} | Issue: {issue}");
+                    _vaildworkshopCount++;
+                }
+
+                Console.Title = $"Workshops {_workshopCount} | Vaild {_vaildworkshopCount} | Invaild {_invailedworkshopCount}";
             }
             else
             {
@@ -79,6 +119,15 @@ namespace WorkshopChecker
                 }
             }
         }
+
+        private static async Task<string> GetHTTPRequestAsync(string url)
+        {
+            using (HttpResponseMessage response = await client.GetAsync(url).ConfigureAwait(false))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
+        }
         static bool hasFile()
         {
             return File.Exists(_path);
@@ -86,7 +135,7 @@ namespace WorkshopChecker
 
         static void sampleCreateFile()
         {
-            File.WriteAllText(_path, "3044705007;2883755057;2899612344");
+            File.WriteAllText(_path, "3044705007;2883755057;2899612344;2016648690");
         }
     }
 }
--- WorkshopChecker/obj/Release/net8.0/WorkshopChecker.AssemblyInfo.cs
+++ WorkshopChecker/obj/Release/net8.0/WorkshopChecker.AssemblyInfo.cs
@@ -12,12 +12,13 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkshopChecker")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright YaMang -w- 2024")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkshopChecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkshopChecker")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
 
